794309e51d86841326ee5f26c5591281990c5b13
AlexBodrov <AlexvBodrov@mail.ru>
2021-11-04 04:56:04 +0300

    add ListView

--- admins/views.py
+++ admins/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth.decorators import user_passes_test
+from django.views.generic.list import ListView
 
 from users.models import User
 from admins.forms import UserAdminRegistrationForm, UserAdminProfileForm
@@ -10,6 +11,11 @@ from admins.forms import UserAdminRegistrationForm, UserAdminProfileForm
 def index(request):
     context = {'title': 'GeekShop - Админ Панель'}
     return render(request, 'admins/index.html', context)
+
+
+class UserListView(ListView):
+    model = User
+    template_name = 'admins/admin-users-read.html'
 
 
 # Create
@@ -27,12 +33,12 @@ def admin_users_create(request):
 
 
 # Read
-@user_passes_test(lambda u: u.is_staff)
-def admin_users(request):
-    context = {'title': 'GeekShop - Пользователи',
-               'users': User.objects.all(),
-               }
-    return render(request, 'admins/admin-users-read.html', context)
+# @user_passes_test(lambda u: u.is_staff)
+# def admin_users(request):
+#     context = {'title': 'GeekShop - Пользователи',
+#                'users': User.objects.all(),
+#                }
+#     return render(request, 'admins/admin-users-read.html', context)
 
 
 # Update
